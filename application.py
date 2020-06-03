f2500385a570bfbb5384e0b06157349915a69faa
kamielgulpen <k.gulpen@hotmail.nl>
2020-06-03 12:06:39 +0200

    added a plot function

--- application.py
+++ application.py
@@ -1,3 +1,11 @@
+import matplotlib.pyplot as plt
+
+def plot(x_coordinates, y_coordinates, x_max, y_max):
+    plt.plot(x_coordinates, y_coordinates, 'ro')
+    plt.axis([0, x_max + 1, 0, y_max + 1])
+    plt.show()
+
+
 def Read(file_name):
     with open (file_name) as f:
         lines = f.read()
