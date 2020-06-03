61d02852fff9e5b4d7653691372e5682be011fb2
thomas vk <thomas_v_k@msn.com>
2020-06-03 12:34:09 +0200

    Implements plot and class

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from models import *
 
 def plot(x_coordinates, y_coordinates, x_max, y_max):
     plt.plot(x_coordinates, y_coordinates, 'ro')
@@ -6,8 +7,9 @@ def plot(x_coordinates, y_coordinates, x_max, y_max):
     plt.grid(linestyle='-', linewidth=0.5)
     plt.show()
 
-# Reads the file and returns list
+
 def Read(file_name):
+    """ Reads the file """
     with open (file_name) as f:
         lines = f.read().splitlines()
     for i in range (len(lines)):
@@ -17,14 +19,18 @@ def Read(file_name):
 if __name__ == '__main__':
     print_0 = Read("gates&netlists/chip_0/print_0.csv")
     netlist_1 = Read("gates&netlists/chip_0/netlist_1.csv")
-    print(print_0)
     x = []
     y = []
+    chips = []
+    counter = 1
     for i in range (len(print_0)):
         if i == 0:
             continue
+        chips.append(Chip(counter, int(print_0[i][1]), int(print_0[i][2])))
         x.append(int(print_0[i][1]))
         y.append(int(print_0[i][2]))
+        counter += 1
     plt = plot(x,y,6,6)
+    
 
     
