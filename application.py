b86546c58e98116e12e919554aa9bd2a6ed7a02a
thomas vk <thomas_v_k@msn.com>
2020-06-03 12:05:16 +0200

    Read Function Complete

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
 def Read(file_name):
     with open (file_name) as f:
-        lines = f.read()
+        lines = f.read().splitlines()
+    for i in range (len(lines)):
+        lines[i] = lines[i].split(',')
     return(lines)
 
 if __name__ == '__main__':
     print_0 = Read("gates&netlists/chip_0/print_0.csv")
-    print(print_0)
+    netlist_1 = Read("gates&netlists/chip_0/netlist_1.csv")
+    
+    
