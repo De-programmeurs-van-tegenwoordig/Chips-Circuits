3ae409f960115bcc467113eca689b7dd30e56a7d
kamielgulpen <k.gulpen@hotmail.nl>
2020-06-04 12:03:10 +0200

    commit

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from models import *
 
 def plot(x_coordinates, y_coordinates, x_max, y_max):
-    ''' makes plot with x ad y inputs (list) '''
+    ''' makes plot with x and y inputs (list) '''
     plt.plot(x_coordinates, y_coordinates, 'ro')
     plt.axis([0, x_max + 1, 0, y_max + 1])
     plt.grid(linestyle='-', linewidth=0.5)
@@ -10,7 +10,7 @@ def plot(x_coordinates, y_coordinates, x_max, y_max):
 
 
 def Read(file_name):
-    """ Reads the file """
+    """ reads the file """
     with open (file_name) as f:
         lines = f.read().splitlines()
     for i in range (len(lines)):
@@ -20,6 +20,7 @@ def Read(file_name):
 if __name__ == '__main__':
     print_0 = Read("gates&netlists/chip_0/print_0.csv")
     netlist_1 = Read("gates&netlists/chip_0/netlist_1.csv")
+    print (netlist_1)
     x = []
     y = []
     chips = []
@@ -31,7 +32,5 @@ if __name__ == '__main__':
         x.append(int(print_0[i][1]))
         y.append(int(print_0[i][2]))
         counter += 1
-    plt = plot(x,y,6,6)
     
-
-    
+    plt = plot(x,y,6,6)    
