a0717e6c009b60937ce54b6e54c24074404cd9ce
thomas vk <thomas_v_k@msn.com>
2020-06-05 12:17:30 +0200

    Adjustments

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -5,7 +5,7 @@ from .chip import Chip
 class Grid():
     def __init__(self, chip_file, netlist_file):
         self.chips = self.load_chips(chip_file)
-        self.netlists = self.load_netlist(netlist_file)
+        self.netlists = self.load_netlists(netlist_file)
 
     def load_chips(self, chip_file):
         """ Reads the file """
@@ -18,8 +18,11 @@ class Grid():
                 new_chip = Chip(count, row['x'], row['y'])
                 chips[int(count)] = new_chip
         return chips
+    
+    def get_chips(self):
+        return self.chips
 
-    def load_netlist(self, netlist_file):
+    def load_netlists(self, netlist_file):
         netlists=[]
         
         with open(netlist_file, 'r') as input_file:
@@ -29,3 +32,6 @@ class Grid():
                 new_connection = [row['chip_a'], row['chip_b']]
                 netlists.append(new_connection)
         return netlists
+    
+    def get_netlists(self):
+        return self.netlists
