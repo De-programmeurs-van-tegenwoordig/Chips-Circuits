f374089616f99c3b530f6d58c8a71cdbaaabde73
Daan Moll <daanmoll@hotmail.com>
2020-06-17 15:54:24 +0200

    mooie mapjes en automatische plotjes

--- main.py
+++ main.py
@@ -11,21 +11,27 @@ import random
 
 if __name__ == '__main__':
     # Read multiple files
-    chip_number = "1"
-    netlist_number = "4"
+    chip_number = "2"
+    netlist_number = "7"
     size = 17
     test_grid = grid.Grid(f"data/chip_{chip_number}/print_{chip_number}.csv", f"data/chip_{chip_number}/netlist_{netlist_number}.csv", size)
 
     output = open("chip2netlistLength.csv", "a")
 
-    reset = False
+    counter = 0
 
-    while not reset:
-        test_grid = grid.Grid(f"data/chip_{chip_number}/print_{chip_number}.csv", f"data/chip_{chip_number}/netlist_{netlist_number}.csv", size)
-        astar = ast.PopAstar(test_grid)
-        reset = astar.run(output)
-        cost = test_grid.cost_of_route()
+    while counter <= 50:
+
+        reset = False
+        counter += 1
+        while not reset:
+            test_grid = grid.Grid(f"data/chip_{chip_number}/print_{chip_number}.csv", f"data/chip_{chip_number}/netlist_{netlist_number}.csv", size)
+            astar = ast.PopAstar(test_grid)
+            reset = astar.run(output)
+            
+        cost = test_grid.cost_of_route()    
         print(cost)
+        pg.plot_grid(test_grid, chip_number, netlist_number, cost)
 
     # Plot the graph
-    pg.plot_grid(test_grid, chip_number, netlist_number, cost)
+    # pg.plot_grid(test_grid, chip_number, netlist_number, cost)
